4894c39e59aa06da47a916c092c46f1b30b8f77f
billchi2 <bmwz2@msn.com>
2022-05-10 15:32:54 +0800

    Update ida.js

--- js/ida.js
+++ js/ida.js
@@ -1,16 +1,15 @@
- $(function(){
+  $(function(){
         csmapi.set_endpoint ('https://farm1.iottalk.tw');
        
         var profile = {
-		'dm_name': 'AS-250',          
-		//'idf_list':[Dummy_Sensor],
-       //'odf_list':[Dummy_Control],
-       'idf_list':[[Dummy_Sensor,'SoilMoisture', 'SoilTemperature', 'SoilConductivity', 'SoilTDS', 'SoilPH', 'atmospheric temperature', 'Atmospheric humidity', 'Atmospheric pressure', 'carbon dioxide', 'luminosity', 'infrared', 'UVI']],
-       'odf_list':[Dummy_Control],
+		'dm_name': 'Dummy_Device',          
+		'idf_list':[Dummy_Sensor],
+              'odf_list':[Dummy_Control],
 		'd_name': undefined,
         };
 			
         function Dummy_Sensor(){
+                 return ['SoilMoisture', 'SoilTemperature', 'SoilConductivity', 'SoilTDS', 'SoilPH', 'atmospheric temperature', 'Atmospheric humidity', 'Atmospheric pressure', 'carbon dioxide', 'luminosity', 'infrared', 'UVI'];
                        }
 
         function Dummy_Control(data){
